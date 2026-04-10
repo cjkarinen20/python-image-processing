af0c5723f802afee54968258c189da292302cb4d
cjkarinen20 <cjkarinen20@gmail.com>
2026-04-10 16:13:23 -0400

    Image Filters
    -Experimented with both basic and advanced
    image filters from the Pillow module ImageFilter.

--- src/pillow-app/main.py
+++ src/pillow-app/main.py
@@ -1,5 +1,5 @@
 import os
-from PIL import Image, ImageEnhance
+from PIL import Image, ImageEnhance, ImageFilter
 
 
 # Get the path of the images directory
@@ -31,9 +31,9 @@ images_dir = os.path.dirname(__file__)
 # cat_rotated.save('cat_rotated.png', 'png')
 
 # -- Color Basics --
-images_path = os.path.join(images_dir, 'images', 'red.png')
+# images_path = os.path.join(images_dir, 'images', 'red.png')
 
-image = Image.open(images_path)
+# image = Image.open(images_path)
 
 # # Get one pixel
 # print(image.getpixel((0,0)))
@@ -56,15 +56,38 @@ image = Image.open(images_path)
 #         if image.getpixel((x, y))[0] == 0:
 #             image.putpixel((x, y), (200, 20, 20))
 
+# -- Image Enhancers --
+# images_path = os.path.join(images_dir, 'images', 'cat.jpg')
+
+# image = Image.open(images_path)
+
 # Create an image enhancer
-vibrance_enhancer = ImageEnhance.Color(image)
-contrast_enhancer = ImageEnhance.Contrast(image)
-brightness_enhancer = ImageEnhance.Brightness(image)
-sharpness_enhancer = ImageEnhance.Sharpness(image)
+# vibrance_enhancer = ImageEnhance.Color(image)
+# contrast_enhancer = ImageEnhance.Contrast(image)
+# brightness_enhancer = ImageEnhance.Brightness(image)
+# sharpness_enhancer = ImageEnhance.Sharpness(image)
 
-# Apply the enhancer
-enhanced_image = sharpness_enhancer.enhance(1.5)
+# # Apply the enhancer
+# enhanced_image = sharpness_enhancer.enhance(1.5)
 
-# Display the image
-image.show()
-enhanced_image.show()
+# # Display the image
+# image.show()
+# enhanced_image.show()
+
+# -- Image Filters --
+images_path = os.path.join(images_dir, 'images', 'cat.jpg')
+
+image = Image.open(images_path)
+
+# Apply basic image filters
+image_blur = image.filter(ImageFilter.BLUR)
+image_contour = image.filter(ImageFilter.CONTOUR)
+image_emboss = image.filter(ImageFilter.EMBOSS)
+image_edge = image.filter(ImageFilter.FIND_EDGES)
+
+# Apply advanced image filters
+image_boxblur = image.filter(ImageFilter.BoxBlur(radius = 20))
+image_gaussianblur = image.filter(ImageFilter.GuassianBlur(radius = 20))
+image_unsharp = image.filter(ImageFilter.UnsharpMask(radius = 20))
+
+image_unsharp.show()
